7cb60d679e6a8a296a010231d4a6341ab3d43429
MD-ABDULLAH-FARDUS <abdullahferdus@gmail.com>
2021-04-05 15:55:28 +0600

    commit

--- Modules/Built_in_modules.js
+++ Modules/Built_in_modules.js
@@ -3,9 +3,18 @@ const fs = require('fs');
 
 const fileSystem = () => {
     // Create file
-    fs.writeFile('demo.txt','This is demo text',(err)=>{
+    fs.writeFile('demo.txt','This is demo text. ',(err)=>{
        return (err ? console.log('err') : console.log('successful'))
     })
+    // Update file
+    fs.appendFile('demo.txt','This text is included by appendFile method.',(err)=>{
+       return (err ? console.log('err') : console.log('successful'))
+    })
+    // Read file
+    fs.readFile('demo.txt','utf-8',(err, data)=>{
+       return (err ? console.log('err') : console.log(data))
+    })
+    
 }
 
 
